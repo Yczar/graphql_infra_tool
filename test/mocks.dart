39c87e1d68e1f34d748cf3d2c6daedd1004f5b40
Benjamin Masebinu <haxeboom@gmail.com>
2026-05-29 02:00:50 +0100

    Updated mocks

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,5 +1,6 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:graphql_infra_tool/config/src/gql_auth_provider.dart';
-import 'package:graphql_infra_tool/config/src/gql_exception_provider.dart';
+import 'package:graphql_infra_tool/config/src/gql_exception_parser.dart';
 import 'package:graphql_infra_tool/exceptions/src/gql_error_model.dart';
 import 'package:graphql_infra_tool/exceptions/src/gql_exceptions.dart';
 
@@ -11,18 +12,17 @@ class MockAuthProvider implements GQLAuthProvider {
   TokenCallback get getToken => () async => 'Bearer mock-token';
 }
 
-class MockExceptionProvider implements GQLExceptionProvider {
+class MockExceptionParser extends GQLExceptionParser {
   @override
-  String get errorCode => 'CUSTOM_ERROR';
+  GQLException? parse(dynamic rawException) {
+    if (rawException is! OperationException) return null;
+    if (rawException.graphqlErrors.isEmpty) return null;
 
-  @override
-  GQLException? createException(
-    String errorCode,
-    String? errorMessage,
-    Map<String, dynamic>? extensions,
-  ) {
+    final code = rawException.graphqlErrors[0].extensions?['code'];
+    if (code != 'CUSTOM_ERROR') return null;
+
     return AppError(
-      AppErrorModel(message: 'Custom handled error', code: errorCode),
+      AppErrorModel(message: 'Custom handled error', code: 'CUSTOM_ERROR'),
     );
   }
 }
